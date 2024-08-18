845c56dd0b849a64aeeec4181fdae1de067d273a
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-08-18 13:41:20 -0300

    [ JS - CSS ] - Deslizamento de tela ao clicar nas ancoras no topo

---
+++ index.js
@@ -0,0 +1,10 @@
+/* função para deslizamento de tela ao clicar nas ancoras no topo */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
