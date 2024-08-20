bdb4b0f6db2a5215788ad7b7632362877fe8d4d2
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-08-20 18:13:06 -0300

    [ REFACTOR ] - <Animacao > das ancoras redes do topo e dos cards da section doacao

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
   });
+
+function pegaDOM(varTag, nameTag){
+   varTag = document.querySelector(nameTag)
+   return varTag
+}
+function apagaClass(){
+  pegaDOM('divAdote', '.card-doe').classList.remove('animate')    
+  pegaDOM('divApadrinhe', '.card-apadrinhe').classList.remove('animate')
+  pegaDOM('divDoe', '.card-adote').classList.remove('animate')
+
+}
+pegaDOM('ancoraAdote', '.menu_link-doacao').addEventListener("click", ()=>{
+  pegaDOM('divAdote', '.card-doe').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+pegaDOM('ancoraApadrinhe', '.menu_link-Apadrinhe').addEventListener("click", ()=>{
+  pegaDOM('divApadrinhe', '.card-apadrinhe').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+pegaDOM('ancoraDoe', '.menu_link-Adote').addEventListener("click", ()=>{
+  pegaDOM('divDoe', '.card-adote').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+
+    
