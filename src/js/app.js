d3da7c3d9dcf3fe59fe8709f76d9a682afd01a4e
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-08-24 16:47:13 -0300

    [ REFACTOR ] pastas e arquivos [ PERF ] <HOME> (header-menu, banner) Responsividade mobile

---
+++ src/js/app.js
@@ -0,0 +1,45 @@
+
+export{getDOM}
+import { menuMobile } from "./menuMobile.js";
+
+
+/* função para deslizamento de tela ao clicar nas ancoras no topo */
+document.querySelectorAll('a[href^="html#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+function getDOM(varTag, nameTag){
+   varTag = document.querySelector(nameTag)
+   return varTag
+}
+/* ============= HOME ============= */
+menuMobile()
+function apagaClass(){
+  getDOM('divAdote', '.card-doe').classList.remove('animate')    
+  getDOM('divApadrinhe', '.card-apadrinhe').classList.remove('animate')
+  getDOM('divDoe', '.card-adote').classList.remove('animate')
+}
+getDOM('ancoraAdote', '.menu_link-doacao').addEventListener("click", ()=>{
+  getDOM('divAdote', '.card-doe').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+getDOM('ancoraApadrinhe', '.menu_link-Apadrinhe').addEventListener("click", ()=>{
+  getDOM('divApadrinhe', '.card-apadrinhe').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+getDOM('ancoraDoe', '.menu_link-Adote').addEventListener("click", ()=>{
+  getDOM('divDoe', '.card-adote').classList.add('animate')
+  setTimeout(apagaClass, 1700);
+})
+/* ============= /// ============= */
+
+
+
+
+
