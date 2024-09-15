b73a78c94d4d1a199cff97288ab21b82b021932d
KELVIN HENRIQUE <kelbaker56@gmail.com>
2024-09-14 23:15:24 -0300

    qr code pix para valores fixos 25/50/100

--- assets/script/modal.js
+++ assets/script/modal.js
@@ -1,46 +1,58 @@
-export { modalMan }
-import { getDOM } from "./index.js";
+import { getDOM } from "./index.js"
 
 function modalMan() {
-    /* alterando valores */
-    getDOM('doacao25', '#doacao25').addEventListener('click', function () {
-        /*    Pix Copia e Cola IMG-QR */
-        getDOM('img-pix', '.img-pix_qr').setAttribute('src', "../images/icons/DOACAO25.png")
-        /* Pix Copia e Cola num */
-        getDOM('qrCodeNum', '.qrCodeNum').innerHTML = `00020126480014br.gov.bcb.pix0114204605560001780208D  OACAO25<br>520400005303986540525.005802BR5901N6001C62070503***6304CB8A`
-    })
+    const qrCodeElements = {
+        '25': {
+            imgSrc: "../images/icons/DOACAO25.png",
+            text: `00020126480014br.gov.bcb.pix0114204605560001780208D  OACAO25<br>520400005303986540525.005802BR5901N6001C62070503***6304CB8A`
+        },
+        '50': {
+            imgSrc: "../images/icons/DOACAO50.png",
+            text: `00020126480014br.gov.bcb.pix0114204605560001780208DOACAO50<br>520400005303986540550.005802BR5901N6001C62070503***630447AF`
+        },
+        '100': {
+            imgSrc: "../images/icons/DOACAO100.png",
+            text: `00020126490014br.gov.bcb.pix0114204605560001780209DOACAO1005<br>204000053039865406100.005802BR5901N6001C62070503***630410B6`
+        },
+        'outros': {
+            imgSrc: "./../images/img/DOACAOoutrosValores.png",
+            text: `00020126510014br.gov.bcb.pix0114204605560001780211DOACAO-SJPA<br>5204000053039865802BR5901N6001C62070503***6304F2B5`
+        }
+    }
 
-    getDOM('doacao50', '#doacao50').addEventListener('click', function () {
-        /*    Pix Copia e Cola IMG-QR */
-        getDOM('img-pix', '.img-pix_qr').setAttribute('src', "../images/icons/DOACAO50.png")
-        /* Pix Copia e Cola num */
-        getDOM('qrCodeNum', '.qrCodeNum').innerHTML = `00020126480014br.gov.bcb.pix0114204605560001780208DOACAO50<br>520400005303986540550.005802BR5901N6001C62070503***630447AF`
-    })
+    function updateQRData(key) {
+        const qrData = qrCodeElements[key]
+        getDOM('img-pix', '.img-pix_qr').setAttribute('src', qrData.imgSrc)
+        getDOM('qrCodeNum', '.qrCodeNum').innerHTML = qrData.text
+    }
 
-    getDOM('doacao100', '#doacao100').addEventListener('click', function () {
-        /*    Pix Copia e Cola IMG-QR */
-        getDOM('img-pix', '.img-pix_qr').setAttribute('src', "../images/icons/DOACAO100.png")
-        /* Pix Copia e Cola num */
-        getDOM('qrCodeNum', '.qrCodeNum').innerHTML = `00020126490014br.gov.bcb.pix0114204605560001780209DOACAO1005<br>204000053039865406100.005802BR5901N6001C62070503***630410B6`
-    })
-
-    getDOM('btnDoacao', '.btnDoacao_outrosValores').addEventListener('click', function () {
-        /*    Pix Copia e Cola IMG-QR */
-        getDOM('img-pix', '.img-pix_qr').setAttribute('src', "./../images/img/DOACAOoutrosValores.png")
-        /* Pix Copia e Cola num */
-        getDOM('qrCodeNum', '.qrCodeNum').innerHTML = `00020126510014br.gov.bcb.pix0114204605560001780211DOACAO-SJPA<br>5204000053039865802BR5901N6001C62070503***6304F2B5`
-    })
+    getDOM('doacao25', '#doacao25').addEventListener('click', () => updateQRData('25'))
+    getDOM('doacao50', '#doacao50').addEventListener('click', () => updateQRData('50'))
+    getDOM('doacao100', '#doacao100').addEventListener('click', () => updateQRData('100'))
+    getDOM('btnDoacao', '.btnDoacao_outrosValores').addEventListener('click', () => updateQRData('outros'))
 
     getDOM('qrCodeNum', '.qrCodeNum').addEventListener('click', function (e) {
-        e.preventDefault(); // Evita o comportamento padrão do link
-        var conteudo = getDOM('qrCodeNum', '.qrCodeNum').innerText;
-        navigator.clipboard.writeText(conteudo).then(function () {
+        e.preventDefault()
+        const conteudo = this.innerText
+        navigator.clipboard.writeText(conteudo).then(() => {
             getDOM('dropdown', '.dropdown').innerHTML = `<p class='tester'>Pix [Copia e cola] copiado com sucesso!</p>`
             alert('Pix [Copia e cola] copiado com sucesso')
-            location.reload();
-
-        }, function (err) {
-            getDOM('dropdown', '.dropdown').innerHTML = `<p>Desculpe! aconteceu um erro! Nos comunique para que podemos concertar.</p>`
-        });
+        }).catch(() => {
+            getDOM('dropdown', '.dropdown').innerHTML = `<p>Desculpe! Aconteceu um erro! Nos comunique para que possamos consertar.</p>`
+        })
     })
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('.btnDoacao_outrosValores')
+    const qrImage = document.getElementById('pix-qr')
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            const qrCodeValue = this.getAttribute('data-qr')
+            qrImage.src = `../images/icons/DOACAO${qrCodeValue}.png`
+        })
+    })
+})
+
+export { modalMan }
