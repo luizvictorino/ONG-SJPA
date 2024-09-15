492838112d6e4dab4f40aa2a1fe0f24168282e2b
Douglas J. Carvalho <dev-douglas@outlook.com.br>
2024-09-14 21:31:57 -0300

    [ feat ], [ fix ]  - adicionar responsividade mobile à página home, devolvendo interacao com modal pix na page adocao

---
+++ assets/script/index.js
@@ -0,0 +1,20 @@
+export{getDOM}
+import { modalMan } from "./modal.js"
+import {menuMobile} from "./responsive.js"
+/* função para deslizamento de tela ao clicar nas ancoras no topo */
+document.querySelectorAll('a[href^="html#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+function getDOM(varTag, nameTag){
+   varTag = document.querySelector(nameTag)
+   return varTag
+}
+menuMobile()
+modalMan
