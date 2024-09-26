84246cd5e93edf02fb1067337df3b467f7238bbc
Maria Fernanda <mfmelo.de@gmail.com>
2024-09-26 15:19:03 -0300

    Inicio ajuste responsividade Mobile pageAdocao ainda não concluida

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,20 +1,21 @@
-export{getDOM}
-import { modalMan } from "./modal.js"
-import {menuMobile} from "./responsive.js"
+export { getDOM };
+import { modalMan } from "./modal.js";
+import { menuMobile } from "./responsive.js";
+
 /* função para deslizamento de tela ao clicar nas ancoras no topo */
-document.querySelectorAll('a[href^="html#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
+document.querySelectorAll('a[href^="html#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
 });
 
-function getDOM(varTag, nameTag){
-   varTag = document.querySelector(nameTag)
-   return varTag
+function getDOM(varTag, nameTag) {
+  varTag = document.querySelector(nameTag);
+  return varTag;
 }
-menuMobile()
-modalMan()
+menuMobile();
+modalMan();
