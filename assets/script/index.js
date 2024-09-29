8baa70d259023a3be2b12d7fe69f34b095cfdb28
luizvictorino <luiz.apvictorino@gmail.com>
2024-09-29 01:42:45 -0300

    Finalização FAQ

--- assets/script/index.js
+++ assets/script/index.js
@@ -18,23 +18,38 @@ function getDOM(varTag, nameTag) {
   return varTag;
 }
 
-document.querySelectorAll(".faq-btn").forEach(function(button) {
-  button.addEventListener("click", function() {
-      var targetId = this.getAttribute("data-target");
-      var targetDiv = document.getElementById(targetId);
+const buttons = document.querySelectorAll(".faq-btn");
+const divs = document.querySelectorAll(".faq-answer");
 
-      // Verificar se o faq ta aberto
-      if (targetDiv.style.display === "block") {
-          targetDiv.style.display = "none";
-      } else {
-          // Ocultar os faqs
-          document.querySelectorAll(".faq-answer").forEach(function(div) {
-              div.style.display = "none";
-          });
-          targetDiv.style.display = "block";
-      }
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+
+    var targetId = this.getAttribute("data-target");
+    var targetDiv = document.getElementById(targetId);
+    var icon = this.querySelector('i');
+
+    // Verificar se o faq atual está aberto
+    if (targetDiv.style.display === "block") {
+      targetDiv.style.display = "none";
+      icon.classList.remove('fa-minus');
+      icon.classList.add('fa-plus');
+    } else {
+      // Ocultar todas as respostas e redefinir ícones para "+"
+      divs.forEach((div, index) => {
+        div.style.display = "none";
+        buttons[index].querySelector('i').classList.remove('fa-minus');
+        buttons[index].querySelector('i').classList.add('fa-plus');
+      });
+      
+      // Mostrar a resposta do botão clicado e mudar o ícone para "-"
+      targetDiv.style.display = "block";
+      icon.classList.remove('fa-plus');
+      icon.classList.add('fa-minus');
+    }
   });
 });
+
+
 menuMobile()
 modalMan()
 
